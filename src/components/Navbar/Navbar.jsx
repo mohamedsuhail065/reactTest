2240f037b941dd2380bb3e4bf240ffc5d24c12d1
mohamedsuhail065 <mohamedsuhail065@gmail.com>
2025-04-20 20:12:49 +0530

    `Update Navbar component title`

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar">
-        <h1>React Test</h1>
+        <h1>React app for user login</h1>
         </div>
   );
 };
